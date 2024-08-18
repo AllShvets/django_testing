4845e8562e69589eb9339414f5ca6feb3794896c
AllShvets <freeskay045@gmail.com>
2024-08-19 00:56:34 +0300

    Привела к ААА

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -15,8 +15,8 @@ def test_anonymous_user_cannot_post_comment(client, detail_page_news_url):
     form_data = FORM_DATA_COMMENT
 
     client.post(detail_page_news_url, data=form_data)
-
     count_comments = Comment.objects.count()
+
     assert count_comments == initial_count_comments
 
 
@@ -36,12 +36,10 @@ def test_auth_user_can_post_comment(
     )
 
     assertRedirects(response, f'{detail_page_news_url}#comments')
-
     comments_count = Comment.objects.count()
-    assert comments_count == initial_count_commit + 1
-
     comment = Comment.objects.get()
 
+    assert comments_count == initial_count_commit + 1
     assert comment.text == form_data['text']
     assert comment.news == test_news
     assert comment.author == sample_author
@@ -64,6 +62,7 @@ def test_user_cannot_submit_comments_with_prohibited_words(
         detail_page_news_url,
         data=comment_data
     )
+    current_comment_count = Comment.objects.count()
 
     assertFormError(
         response,
@@ -71,8 +70,6 @@ def test_user_cannot_submit_comments_with_prohibited_words(
         field='text',
         errors=WARNING
     )
-
-    current_comment_count = Comment.objects.count()
     assert current_comment_count == initial_comment_count
 
 
@@ -94,8 +91,8 @@ def test_user_can_edit_own_comment(
     )
 
     assertRedirects(response, f'{detail_page_news_url}#comments')
-
     sample_comment.refresh_from_db()
+
     assert sample_comment.text == new_comment_text
 
 
@@ -109,12 +106,11 @@ def test_author_can_delete_own_comment(
     comments_url = detail_page_news_url + '#comments'
 
     response = authenticated_author_client.delete(delete_comment_url)
+    comment_id = delete_comment_url.split('/')[-2]
 
     assert response.status_code == HTTPStatus.FOUND
     assert response.url == comments_url
     assert Comment.objects.count() == initial_comment_count - 1
-
-    comment_id = delete_comment_url.split('/')[-2]
     assert not Comment.objects.filter(id=comment_id).exists()
 
 
@@ -142,6 +138,6 @@ def test_user_cant_delete_comment_of_another_user(
     comments_count_init = Comment.objects.count()
 
     authenticated_not_author_client.delete(delete_comment_url)
-
     comments_count = Comment.objects.count()
+
     assert comments_count == comments_count_init
